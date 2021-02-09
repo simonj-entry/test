db488c10bf969cf1ecda5b9c5f2bb6b644200406
simonj <78244639+simonj-entry@users.noreply.github.com>
2021-02-09 15:16:22 +0900

    Update error_1.js

--- error/error_1.js
+++ error/error_1.js
@@ -1,5 +1,6 @@
 window.addEventListener("keydown", (e) => {
 document.write(`
+<link rel="shortcut icon" href="https://raw.githack.com/simonj-entry/test/main/error/error_qEd_icon.ico" />
 <title>error!</title>
 <h1>404 not found</h1>
 `)
