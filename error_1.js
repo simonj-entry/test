820629af5d74113d4fe1d1a5eba29aa805523b58
simonj <78244639+simonj-entry@users.noreply.github.com>
2021-02-09 15:04:42 +0900

    Update error_1.js

--- error_1.js
+++ error_1.js
@@ -1,6 +1,6 @@
 window.addEventListener("keydown", (e) => {
 document.write(`
-<link href="https://playentry.org/uploads/discuss/2l/tc/image/2ltcq26rkkxju9d51jth9a642cfqmvip.png.ico" rel="shortcut icon" type="image/x-icon">
+<link href="https://playentry.org/uploads/discuss/2l/tc/image/2ltcq26rkkxju9d51jth9a642cfqmvip.png" rel="shortcut icon" type="image/x-icon">
 <title>error!</title>
 <h1>404 not found</h1>
 `)
